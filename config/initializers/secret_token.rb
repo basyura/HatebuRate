b028c5b7c06a926e4be3206339819f6e9c58c25f
basyura <basyura@gmail.com>
2013-03-31 13:07:28 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HatebuRate::Application.config.secret_key_base = '241aa454a2782af0e8e01d5558fc55cf2dce26e017f61bf803ef500fe4541333b2f2f8d35d29f6405a0d00cbfd78876871e1beac96a4890f895e0e3bf35caed3'
