03b8e476508b43be835fbf8feab75b65147420e2
basyura <basyura@gmail.com>
2013-03-31 19:41:12 +0900

    added rank model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,26 @@ class HomeController < ApplicationController
   def index
     @items = HatebuRate::RSS.new.items.map.each{|item| RssItem.new(item) }
   end
+
   def up
-    puts params[:title]
-    puts HatebuRate::Mecab.new.parse(params[:title])
+    rank(1)
     render
+  end
+
+  def down
+    rank(-1)
+    render
+  end
+
+  private
+  def rank(point)
+    HatebuRate::Mecab.new.parse(params[:title]).each do |m|
+      rank = Rank.where(:word => m).first
+      unless rank
+        rank = Rank.new(:word => m, :point => 0)
+      end
+      rank.point += point
+      rank.save
+    end
   end
 end
